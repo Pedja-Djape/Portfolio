cc860403b5b8bbee0e9381e234f09ec664cd149c
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-04 17:29:59 -0400

    finished Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,28 +4,44 @@ import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 const Home = () => {
   return (
     <div
-        name="Home"
-        className='h-screen w-full bg-gradient-to-to-b from-black to-gray-800'
+        name="home"
+        className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
 
-        <div>
-            <div>
-                <h2>
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className="text-4xl sm:text-6xl font-bold text-white">
                     I am 
                 </h2>
-                <p>
+                <p className='text-gray-500 py-4 max-w-md'>
                     Short Summary.
                 </p>
 
                 <div>
-                    <button>
+                    <button 
+                        className='group text-white w-fit px-6 py-3 my-2
+                        flex items-center rounded-md bg-gradient-to-r
+                        from-[#fbc2eb] via-[#a6c1ee] to-[#fbc2eb]
+                        cursor-pointer' >
                         Portfolio
+                    
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <MdOutlineKeyboardArrowRight size={25}
+                            className='ml-1 '
+                            />
+                        </span>
                     </button>
-                    <span>
-                        <MdOutlineKeyboardArrowRight />
-                    </span>
                 </div>
             </div>
+
+            <div>
+                <img 
+                    src={profileImage} 
+                    alt='Me!'
+                    className="rounded-2xl mx-auto w-1/3 "
+                    />
+            </div>
+
         </div>
     </div>
   )
