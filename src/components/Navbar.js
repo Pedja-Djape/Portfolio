65f5338585c796ba8a89e0498021471a73248e81
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-05 12:35:36 -0400

    added Smooth scroll to navbar elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {FaBars, FaTimes } from "react-icons/fa"
 import { useState } from 'react'
+import { Link } from "react-scroll"
 
 
 const Navbar = () => {
@@ -40,11 +41,13 @@ const Navbar = () => {
 			<ul className='hidden md:flex'>
 				{links.map(
 					(link) => 
-						<li 
-							className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'
-							key={link.id}>
-							{link.link}
-						</li>
+						<Link to={link.link} smooth duration={700}>
+							<li 
+								className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200 hover:text-white'
+								key={link.id}>
+								{link.link}
+							</li>
+						</Link >
 					)
 				}
 			</ul>
@@ -61,13 +64,14 @@ const Navbar = () => {
 				<ul 
 					className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
 					{links.map(
-					(link) => 
-						<li 
-							className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'
-							key={link.id}>
-							{link.link}
-						</li>
-					)
+						(link) => 
+							<li 
+								className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'
+								key={link.id}
+							>
+								{link.link}
+							</li>
+						)
 				}
 				</ul>
 			)}
