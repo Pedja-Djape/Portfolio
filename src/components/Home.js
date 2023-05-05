fa82aad9d872bbc78720539e9fc803e63d3e91f8
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-05 12:35:14 -0400

    added smooth scroll and changed button colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import profileImage from "../assets/profile.JPG";
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { Link } from "react-scroll"
+
 const Home = () => {
   return (
     <div
@@ -11,26 +13,31 @@ const Home = () => {
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className='flex flex-col justify-center h-full'>
                 <h2 className="text-4xl sm:text-6xl font-bold text-white">
-                    I am 
+                    I'm a Software Engineer.
                 </h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                     Short Summary.
                 </p>
 
                 <div>
-                    <button 
+                    <Link 
+                        to='portfolio'
+                        smooth
+                        duration={750}
                         className='group text-white w-fit px-6 py-3 my-2
                         flex items-center rounded-md bg-gradient-to-r
                         from-[#fbc2eb] via-[#a6c1ee] to-[#fbc2eb]
-                        cursor-pointer' >
+                        cursor-pointer' 
+                        >
                         Portfolio
-                    
                         <span className='group-hover:rotate-90 duration-300'>
                             <MdOutlineKeyboardArrowRight size={25}
                             className='ml-1 '
                             />
                         </span>
-                    </button>
+                    </Link>
+                        
+                        
                 </div>
             </div>
 
@@ -38,7 +45,7 @@ const Home = () => {
                 <img 
                     src={profileImage} 
                     alt='Me!'
-                    className="rounded-2xl mx-auto w-1/3 "
+                    className="rounded-2xl mx-auto w-2/3 "
                     />
             </div>
 
