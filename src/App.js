615c1ef0575274d237d61bbb5441936f2f7d98b9
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-05 12:33:38 -0400

    Added main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import About from "./components/About";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+import SocialLinks from "./components/SocialLinks";
 
 
 function App() {
 	return (
 		<div>
-			<h1 className="text-4xl font-bold">Hello world!</h1>
+			<Navbar />
+			<Home />
+			<About />
+			<Portfolio />
+			<Skills />
+
+			<SocialLinks />
 		</div>
 	);
 }
