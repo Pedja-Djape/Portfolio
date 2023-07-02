e9c6fa67555b01954c8085c91ea8de031d689e3d
Pedja-Djape <pmuratov1015@gmail.com>
2023-07-02 19:57:26 -0400

    fixed skills z position

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -131,7 +131,7 @@ const Skills = () => {
                             {skills[skillType].map(({ id, src, title, style }) => (
                                 <div
                                     key={id}
-                                    className={`z-10 shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                                    className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
                                 >
                                     <img src={src} alt="" className="w-20 mx-auto" />
                                     <p className="mt-4">{title}</p>
